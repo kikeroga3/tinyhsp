a70101db46daf4074c128875a9db59f56837b598
kikeroga <kikeroga@gmail.com>
2017-02-14 22:59:34 +0900

    Add files via upload

--- sample/maze.hs
+++ sample/maze.hs
@@ -1,18 +1,17 @@
-title "MAZE"
+title "THE MAZE"
 
-	sz=32
-
+	xs=38 :ys=28 :bs=8
 	randomize
-	color :boxf 4,4,8*sz+7,8*sz+7
+	color :boxf bs,bs,bs+xs*bs*2+bs-1,bs+ys*bs*2+bs-1
 	color 255,255,255
-	repeat sz :x=cnt*8+4+4
-		repeat sz :y=cnt*8+4+4
-			boxf x,y,x+3,y+3
+	repeat xs :x=bs+cnt*bs*2+bs
+		repeat ys :y=bs+cnt*bs*2+bs
+			boxf x,y,x+bs-1,y+bs-1
 			a=rnd(4)
-			if a=1 :boxf x,y-4,x+3,y-4+3
-			if a=2 :boxf x+4,y,x+4+3,y+3
-			if a=3 :boxf x,y+4,x+3,y+4+3
-			if a=0 :boxf x-4,y,x-4+3,y+3
+			if a=0 :boxf x,y-bs,x+bs-1,y-bs+bs-1
+			if a=1 :boxf x+bs,y,x+bs+bs-1,y+bs-1
+			if a=2 :boxf x,y+bs,x+bs-1,y+bs+bs-1
+			if a=3 :boxf x-bs,y,x-bs+bs-1,y+bs-1
 		loop
 	loop
 	stop
