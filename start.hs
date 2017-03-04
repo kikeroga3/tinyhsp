e5c16b471ac7396384c1172543f71e222bb826de
kikeroga <kikeroga@gmail.com>
2017-03-04 19:51:24 +0900

    Update start.hs

--- start.hs
+++ start.hs
@@ -5,23 +5,7 @@ old_y = -1
 now_x = 0
 now_y = 0
 
-	color :boxf 0,0,640,480
-	color 255,255,255
-
 repeat
-
-;   1   カーソルキー左(←)
-;   2   カーソルキー上(↑)
-;   4   カーソルキー右(→)
-;   8   カーソルキー下(↓)
-;  16   スペースキー
-;  32   Enterキー
-;  64   Ctrlキー
-; 128   ESCキー
-; 256   マウスの左ボタン
-; 512   マウスの右ボタン
-;1024   TABキー
-
 	stick key
 	if key & 256 {
 		now_x = mousex
@@ -37,4 +21,3 @@ repeat
 loop
 
 stop
-
